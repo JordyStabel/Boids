e32efa1494e2966debb5f00ceae59e1425b8ad11
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-05-14 17:42:29 +0200

    working version of boids, next: quadtree to make it run more effecient

---
+++ boid.js
@@ -0,0 +1,154 @@
+let neighbours = [];
+let noNeighour = true;
+let size = 5;
+
+class Boid {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.acceleration = createVector();
+    this.maxForce = 0.2;
+    this.maxSpeed = 4;
+    this.color = color(0);
+  }
+
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+
+  align(boids) {
+    // Get the average velocity of all surrounding boids in a certain range from the 'main' boid
+    let range = 50;
+    let trajectory = createVector();
+    let inRange = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && distance < range) {
+        trajectory.add(other.velocity);
+        inRange++;
+      }
+    }
+    if (inRange > 0) {
+      // Get the average trajectory
+      trajectory.div(inRange);
+
+      // Substract this trajectory from the average trajectory
+      trajectory.setMag(this.maxSpeed);
+      trajectory.sub(this.velocity);
+      trajectory.limit(this.maxForce);
+    }
+    return trajectory;
+  }
+
+  cohesion(boids) {
+    let range = 100;
+    let trajectory = createVector();
+    let inRange = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && distance < range) {
+        trajectory.add(other.position);
+        inRange++;
+      }
+    }
+    if (inRange > 0) {
+      trajectory.div(inRange);
+      trajectory.sub(this.position);
+      trajectory.setMag(this.maxSpeed);
+      trajectory.sub(this.velocity);
+      trajectory.limit(this.maxForce);
+    }
+    return trajectory;
+  }
+
+  separation(boids) {
+    let range = 100;
+    let trajectory = createVector();
+    let inRange = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && distance < range) {
+        let difference = p5.Vector.sub(this.position, other.position);
+        difference.div(pow(distance, 2));
+        trajectory.add(difference);
+        inRange++;
+        if (!noNeighour) {
+          neighbours.push(other);
+        }
+      } else {
+        neighbours.splice(neighbours.indexOf(other), 1);
+        this.noNeighour = true;
+        this.setColor(0);
+      }
+    }
+    if (inRange > 0) {
+      trajectory.div(inRange);
+      trajectory.setMag(this.maxSpeed);
+      trajectory.sub(this.velocity);
+      trajectory.limit(this.maxForce);
+
+      this.size = 5 + inRange / 4;
+
+      this.color = color(inRange * 10);
+
+      this.setColor(this.color);
+    }
+    return trajectory;
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  flock(boids) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let seperation = this.separation(boids);
+
+    seperation.mult(seperationSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    alignment.mult(alignSlider.value());
+
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(seperation);
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    strokeWeight(this.size);
+    stroke(this.color);
+    point(this.position.x, this.position.y);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+const flock = [];
+let paused = false;
+
+let alignSlider, cohesionSlider, seperationSlider;
+
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(900, 600);
+  alignSlider = createSlider(0, 5, 1, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  seperationSlider = createSlider(0, 2, 1, 0.1);
+  for (let i = 0; i < 5; i++) {
+    flock.push(new Boid(random(width), random(height)));
+  }
+}
+
+function mouseClicked() {
+  flock.push(new Boid(mouseX, mouseY));
+}
+
+function keyPressed() {
+  paused = !paused;
 }
 
 function draw() {
-  background(51);
+  if (paused) {
+    return;
+  }
+
+  background(70, 130, 180);
+
+  if (mouseIsPressed && flock.length < 100) {
+    flock.push(new Boid(mouseX, mouseY));
+  }
+
+  for (let boid of flock) {
+    boid.edges();
+    boid.flock(flock);
+    boid.update();
+    boid.show();
+  }
 }
